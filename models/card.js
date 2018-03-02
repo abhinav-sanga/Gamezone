33fe0f388e71b6f765db19f9dcfb6c74f341e44f
abhi_0515 <abhi.for.u55@gmail.com>
2018-03-02 16:43:19 +0530

    modified app.js

--- models/card.js
+++ models/card.js
@@ -4,8 +4,7 @@ var Schema = mongoose.Schema;
 var cardSchema = new Schema({
     user:{type: Schema.Types.ObjectId, ref: 'User'},
     cardNumber: {type: Number, required: true},
-    amountRecharged: {type: Number, required: true},
-    dateOfRech: {type: String, required: true}
+    amountRecharged: {type: Number, required: true}
 });
 
 module.exports = mongoose.model('Card', cardSchema);
--- routes/card.js
+++ routes/card.js
@@ -2,18 +2,17 @@ var express = require('express');
 var router = express.Router();
 var Card = require('../models/card');
 
-router.post('/rech', function(req,res,next,done){
+router.post('/rech', function(req,res,next){
     var newCard = new Card();
     newCard.user = req.user;
     newCard.cardNumber = req.body.cardnum;
     newCard.amountRecharged = req.body.amountRech;
-    //newCard.dateOfRech = req.body.dateOfRech;
-    newCard.save(function (err, result) {
+    newCard.save(function (err, card) {
         if(err){
-            return done(err);
+            res.send(err);
         }
-        req.flash('success', 'Successfully Recharged!');
-        return done(null, newCard);
+        req.flash('Successfully Recharged!', message);
+        res.send(card);
     });
 });
 
